c5da336b78a80c0b9370ff85a00f3f32284a4f7e
DenverW03 <d.whiteley@lancaster.ac.uk>
2024-01-15 09:31:52 +0000

    issue with it not spawning correct number of robots

--- boids/run_template.sh
+++ boids/run_template.sh
@@ -1,4 +1,5 @@
-export DYLD_LIBRARY_PATH=/opt/homebrew/Cellar/fltk/HEAD-4964a15_1/lib;
+# Ensure that the version number is correct of the fltk library will fail to be found
+export DYLD_LIBRARY_PATH=/opt/homebrew/Cellar/fltk/HEAD-ecb3e40/lib;
 export STAGE_HEADER=/usr/local/include/Stage-4.3;
 export STAGEPATH=/usr/local/lib;
 echo "LINE TO BE REPLACED"
